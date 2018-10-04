3f544db05790f0346f59eb03859147c6367307f0
40180378 <fmcnamee04@qub.ac.uk>
2018-10-04 02:13:00 +0100

    Added source code
    Added source code

---
+++ README.md
@@ -0,0 +1,8 @@
+howCode's Programming Language
+==============================
+
+This is the source code howCode's programming language series.
+
+(Currently the source code just contains the lexer, the source code will be updated as the videos are uploaded.)
+
+You can watch the videos that accompany this series here: https://www.youtube.com/playlist?list=PLBOh8f9FoHHgPEbiK-FSdSw3FiyP78fbk
---
+++ basic_lexer.py
@@ -0,0 +1,48 @@
+from sly import Lexer
+
+class BasicLexer(Lexer):
+    tokens = { NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUN, TO, ARROW, EQEQ }
+    ignore = '\t '
+
+    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';' }
+
+    # Define tokens
+    IF = r'IF'
+    THEN = r'THEN'
+    ELSE = r'ELSE'
+    FOR = r'FOR'
+    FUN = r'FUN'
+    TO = r'TO'
+    ARROW = r'->'
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self,t ):
+        self.lineno = t.value.count('\n')
+
+
+
+if __name__ == '__main__':
+    lexer = BasicLexer()
+    env = {}
+    while True:
+        try:
+            text = input('basic > ')
+        except EOFError:
+            break
+        if text:
+            lex = lexer.tokenize(text)
+            for token in lex:
+                print(token)
